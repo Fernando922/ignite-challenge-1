91ea8365aab24b32ed4be720abc6458659a7f32e
Fernando de Paula <dipaula018@gmail.com>
2021-11-03 22:04:58 -0300

    tests: fix all tests

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,12 @@
 import React, { useState } from "react";
-import { Alert, StyleSheet, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 import { Header } from "../components/Header";
 import { Task, TasksList } from "../components/TasksList";
 import { TodoInput } from "../components/TodoInput";
 
-export type EditTaskArgs = {
-  taskId: number;
-  taskNewTitle: string;
-};
-
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
-
-  function verifyIfTaskExist(title: string): Task | null {
-    const taskAlreadyCreated = tasks.find((task) => task.title === title);
-    if (taskAlreadyCreated) {
-      return taskAlreadyCreated;
-    }
-    return null;
-  }
 
   function handleAddTask(title: string) {
     const newTask = {
@@ -27,15 +14,6 @@ export function Home() {
       title,
       done: false,
     };
-
-    const haveTaskWithSameTitle = verifyIfTaskExist(title);
-
-    if (haveTaskWithSameTitle) {
-      return Alert.alert(
-        "Task já cadastrada",
-        "Você não pode cadastar uma task com o mesmo nome"
-      );
-    }
 
     setTasks([...tasks, newTask]);
   }
@@ -53,28 +31,7 @@ export function Home() {
   }
 
   function handleRemoveTask(id: number) {
-    Alert.alert(
-      "Remover item",
-      "Tem certeza que você deseja remover este item?",
-      [
-        { text: "nao", style: "cancel" },
-        {
-          text: "sim",
-          style: "destructive",
-          onPress: () => setTasks(tasks.filter((task) => task.id !== id)),
-        },
-      ]
-    );
-  }
-
-  function handleEditTask({ taskId, taskNewTitle }: EditTaskArgs) {
-    const updatedTasks = tasks.map((task) => ({ ...task }));
-    updatedTasks.forEach((task) => {
-      if (task.id === taskId) {
-        task.title = taskNewTitle;
-      }
-    });
-    setTasks(updatedTasks);
+    setTasks(tasks.filter((task) => task.id !== id));
   }
 
   return (
@@ -87,7 +44,6 @@ export function Home() {
         tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
         removeTask={handleRemoveTask}
-        editTask={handleEditTask}
       />
     </View>
   );
